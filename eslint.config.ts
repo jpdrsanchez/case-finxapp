9df9499678c81000d95df36a076911516c36882b
jpdrsanchez <joaopedrosanchez302@gmail.com>
2025-12-14 16:22:55 -0300

    chore: eslint and prettier setup

---
+++ eslint.config.ts
@@ -0,0 +1,39 @@
+import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginVue from 'eslint-plugin-vue'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs
+} from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}']
+  },
+  globalIgnores([
+    '**/dist/**',
+    '**/dist-ssr/**',
+    '**/coverage/**',
+    'junit.xml',
+    '**/node_modules/**',
+    '!.storybook',
+    '**/mockServiceWorker.js'
+  ]),
+
+  js.configs.recommended,
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommendedTypeChecked,
+
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser
+    }
+  },
+
+  skipFormatting
+)
