02a5c77ba1203ca0cdf4e2d28376aec3055167bf
jpdrsanchez <joaopedrosanchez302@gmail.com>
2025-12-14 20:00:03 -0300

    chore: nuxt-ui lib setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ui({
+      colorMode: false,
+      ui: { colors: { primary: 'brand-primary', secondary: 'brand-secondary' } }
+    })
+  ],
   test: {
     environment: 'happy-dom',
     setupFiles: 'test/vitest/setup.ts',
